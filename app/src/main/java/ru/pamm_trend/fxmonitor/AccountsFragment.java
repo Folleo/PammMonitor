da3426ecdd510a627f2e636132694fbae3a02b89
dkolotev <Daniil.Kolotev@cbossgroup.com>
2015-03-20 10:12:20 +0300

    Added blank DetailActivity and Detail Fragment

---
+++ app/src/main/java/ru/pamm_trend/fxmonitor/AccountsFragment.java
@@ -0,0 +1,23 @@
+package ru.pamm_trend.fxmonitor;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dkolotev on 20.03.2015.
+ */
+public class AccountsFragment extends Fragment {
+    public AccountsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+}
--- app/src/main/java/ru/pamm_trend/fxmonitor/DetailActivity.java
+++ app/src/main/java/ru/pamm_trend/fxmonitor/DetailActivity.java
@@ -1,40 +1,24 @@
 package ru.pamm_trend.fxmonitor;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    private static final String DETAILFRAGMENT_TAG = "DFTAG";
-    private final String LOG_TAG = MainActivity.class.getSimpleName();
-    boolean mTwoPane;
+public class DetailActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Authorize user if there are no login and api key in shared preferences
-        if (!PrefUtils.isSavedCredentials(this)) {
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        }
-
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new AccountsFragment(), DETAILFRAGMENT_TAG)
-                    .commit();
-        }
+        setContentView(R.layout.activity_detail);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
@@ -52,5 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
